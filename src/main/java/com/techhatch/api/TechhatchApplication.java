6a7210e03d095136fce5021816006ad1c9c63ff8
codephobe <kutakanakerisanjay@gmail.com>
2025-11-10 22:45:40 +0530

    Spring Boot maven setup

---
+++ src/main/java/com/techhatch/api/TechhatchApplication.java
@@ -0,0 +1,13 @@
+package com.techhatch.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechhatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechhatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techhatch/api/TechhatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techhatch.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechhatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
