080b366a0c6640f76f05eb5160417e196d0906db
codephobe <kutakanakerisanjay@gmail.com>
2025-11-15 13:43:03 +0530

    fix : validateToken() now validates correctly and removed KeyGenerator with actual key from env variable.

--- src/main/java/api/techhatch/com/util/JwtUtil.java
+++ src/main/java/api/techhatch/com/util/JwtUtil.java
@@ -1,18 +1,13 @@
 package api.techhatch.com.util;
 
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
-
-import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
-import java.security.NoSuchAlgorithmException;
-import java.util.Base64;
 import java.util.Date;
 import java.util.function.Function;
 
@@ -24,18 +19,6 @@ public class JwtUtil {
 
     @Value("${jwt.expiration}")
     private Long EXPIRATION_TIME;
-
-    private String secret="";
-
-    public JwtUtil(){
-        try{
-            KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
-            SecretKey key = keyGen.generateKey();
-            secret = Base64.getEncoder().encodeToString(key.getEncoded());
-        }catch (NoSuchAlgorithmException e){
-            throw new RuntimeException(e);
-        }
-    }
 
     public String generateToken(String email, String role, Long userId){
         System.out.println("Generating token");
@@ -50,21 +33,21 @@ public class JwtUtil {
     }
 
     SecretKey getKey(){
-        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
-    public boolean validateToken(String token, UserDetails email){
-        try{
-            Jwts.parser()
-                    .verifyWith(getKey())
-                    .build()
-                    .parseSignedClaims(token);
-            return true;
-        }catch (JwtException e){
-            System.out.println(e.getMessage());
-            return false;
-        }
+    public boolean validateToken(String token, UserDetails userDetails){
+        String email = getEmailFromToken(token);
+        return (email.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
     }
 
     private <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
