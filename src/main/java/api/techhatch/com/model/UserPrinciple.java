90f2b3a71627e41b3e3c005feee6722982291063
codephobe <kutakanakerisanjay@gmail.com>
2025-11-16 23:51:53 +0530

    fix : added 'ROLE_' prefix for roles as  it is standardised in Spring Security.

--- src/main/java/api/techhatch/com/model/UserPrinciple.java
+++ src/main/java/api/techhatch/com/model/UserPrinciple.java
@@ -16,7 +16,7 @@ public class UserPrinciple implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.singleton(new SimpleGrantedAuthority(user.getRole().toString()));
+        return Collections.singleton(new SimpleGrantedAuthority("ROLE_"+user.getRole().toString()));
     }
 
     @Override
